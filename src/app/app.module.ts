191b58eb893c8a65405fcc736339dbbc0e3afd8f
Camilo Davila <camilo7davila@gmail.com>
2020-09-04 13:33:14 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IngesoEgresoComponent } from './ingeso-egreso/ingeso-egreso.component';
+import { EstadisticaComponent } from './ingeso-egreso/estadistica/estadistica.component';
+import { DetalleComponent } from './ingeso-egreso/detalle/detalle.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IngesoEgresoComponent,
+    EstadisticaComponent,
+    DetalleComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from "@angular/router";
+
+import { EstadisticaComponent } from "../ingeso-egreso/estadistica/estadistica.component";
+import { DetalleComponent } from "../ingeso-egreso/detalle/detalle.component";
+import { IngesoEgresoComponent } from "../ingeso-egreso/ingeso-egreso.component";
+
+export const dashboardRoutes: Routes = [
+    { path: '', component: EstadisticaComponent },
+    { path: 'ingreso-egreso', component: IngesoEgresoComponent },
+    { path: 'detalle', component: DetalleComponent },
+
+]
